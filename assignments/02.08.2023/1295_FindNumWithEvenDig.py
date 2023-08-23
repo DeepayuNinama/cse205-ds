f19d50bf7bfc2bc5925050363df4a73950149d5b
DeepayuNinama <138458194+DeepayuNinama@users.noreply.github.com>
2023-08-23 14:22:49 +0530

    .git push

---
+++ assignments/02.08.2023/1295_FindNumWithEvenDig.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+
+        count = 0
+        for i in nums:
+
+            ele = str(i)
+            if len(ele) % 2 ==0:
+                count  += 1
+
+        return count   
